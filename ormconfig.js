312806e2291bd1a9c36338a6a9e9836969c47493
Ignacio Gonzalez Chemes <nachogonza123@gmail.com>
2022-05-24 21:57:56 -0300

    Modifico entrypoint

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,7 @@ const NestEnvConfiguration = require('./src/Configs/NestEnvConfig');
 const { ModulesContainer } = require('@nestjs/core');
 
 let envData = Dotenv.config({ path: `${path.join(__dirname)}/${EnvConfiguration.envFilePathConfiguration()}` }).parsed;
-console.log(`\u001b[36mTYPEORM ENVIRONMENT: ${process.env.REG}\nDATABASE CONNECTION: ${envData.DATABASE_HOST}\u001b[39m`);
+console.log(`\u001b[36mTYPEORM ENVIRONMENT: ${process.env.REG}\nDATABASE CONNECTION: ${process.env.DATABASE_HOST}\u001b[39m`);
 let envs = NestEnvConfiguration.envModelTransformer(envData);
 module.exports = {
     ...envs.DATABASE,
