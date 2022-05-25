0d7f63d352a752a76f9b2ad5952dc67c7c0a66b1
Ignacio Gonzalez Chemes <nachogonza123@gmail.com>
2022-05-24 22:39:16 -0300

    Modifico entrypoint

--- src/Configs/NestEnvConfig.ts
+++ src/Configs/NestEnvConfig.ts
@@ -17,7 +17,7 @@ export const envModelTransformer = (envs: any) => ({
     MINIO: {
         endPoint: process.env.MINIO_ENDPOINT,
         port: parseInt(process.env.MINIO_PORT, 10),
-        useSSL: process.env.MINIO_USE_SSL,
+        useSSL: false,
         accessKey: process.env.MINIO_ACCESS_KEY,
         secretKey: process.env.MINIO_SECRET_KEY,
     },
